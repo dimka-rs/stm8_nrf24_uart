66688b1d33c19b52231426a9e02758060c4d70a2
Dmitry K <dimka.rs@gmail.com>
2018-02-18 16:00:20 +0300

    renamed SPI_CS to NRF_CS

--- src/main.h
+++ src/main.h
@@ -4,10 +4,8 @@
 #define PORT_LED GPIOB
 #define PIN_LED GPIO_PIN_5
 
-#define PORT_SPI GPIOC
-#define PIN_SPI_CS GPIO_PIN_4
-
 #define PORT_NRF GPIOC
+#define PIN_NRF_CS GPIO_PIN_4
 #define PIN_NRF_CE GPIO_PIN_3
 
 /*
